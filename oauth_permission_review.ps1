74195d977ce9b2e52e4a65b2dda9829797303de6
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2024-02-23 11:46:53 -0800

    Update oauth_permission_review.ps1

--- oauth_permission_review.ps1
+++ oauth_permission_review.ps1
@@ -1,20 +1,33 @@
 # Define a mapping of dangerous permission IDs to human-readable names
 $permissionMappings = @{
-    "7427e0e9-2fba-42fe-b0c0-848c9e6a8182" = "Maintain access to data you have given access to";
-    "e1fe6dd8-ba31-4d61-89e7-88639da4683d" = "Sign in and read user profile";
-    "863451e7-0667-486c-a5d6-d135439485f0" = "Have full access to all files user can access";
-    "17dde5bd-8c17-420f-a486-969730c1b827" = "Read and write files that the user selects";
-    "df85f4d6-205c-4ac5-a5ea-6bf408dba283" = "Read all files that the user can access";
-    "b4e74841-8e56-480b-be8b-910348b18b4c" = "Read and write access to user's profile";
     "024d486e-b451-40bb-833d-3e66d98c5c73" = "Read and write access to user's email";
-    "d56682ec-c09e-4743-aaf4-1a3aac4caa21" = "Read and write access to user's contacts";
-    "2219042f-cab5-40cc-b0d2-16b1540b4c5f" = "Read and write access to user's tasks";
-    "1ec239c2-d7c9-4623-a91a-a9775856bb36" = "Read and write access to user's calendars";
-    "a154be20-db9c-4678-8ab7-66f6cc099a59" = "Sign in and read all user profiles";
-    "ba47897c-39ec-4d83-8086-ee8256fa737d" = "Read user's people data";
+    "570282fd-fa5c-430d-a7fd-fc8dc98a9dca" = "Read the users email";
+    "7b9103a5-4610-446b-9670-80643382c1fa" = "Read user and shared email";
+    "a4b8392a-d8d1-4954-a029-8e668a39a170" = "Read users basic mail";
+    "b11fa0e7-fdb7-4dc9-b1f1-59facd463480" = "Read users and basic shared mail";
+    "5df07973-7d5d-46ed-9847-1271055cbd51" = "Read and write user and shared mail";
+    "e383f46e-2787-4529-855e-0e479a3ffac0" = "Send Mail as the User";
+    "a367ab51-6b49-43bf-a716-a1fb06d2a174" = "Send Mail on behalf of others";
+    "ff74d97f-43af-4b68-9f2a-b77ee6968c5d" = "Read user contacts";
+    "242b9d9e-ed24-4d09-9a52-f43769beb9d4" = "Read user and shared contacts";
+    "d56682ec-c09e-4743-aaf4-1a3aac4caa21" = "Have full access to user contacts";
+    "afb6c84b-06be-49af-80bb-8f3f77004eab" = "Read and write user and shared contacts";
+    "87f447af-9fa4-4c32-9dfa-4a57a73d18ce" = "Read user's mailbox settings";
     "818c620a-27a9-40bd-a6a5-d96f7d610b4b" = "Read and write access to user's mailbox settings";
-    "64ac0503-b4fa-45d9-b544-71a463f05da0" = "Read and write access to user's notes";
-    "c5366453-9fb0-48a5-a156-24f0c49a4b84" = "Read and write access to user's directory";
+    "ba47897c-39ec-4d83-8086-ee8256fa737d" = "Read user's people data";
+    "b89f9189-71a5-4e70-b041-9887f0bc7e4a" = "Read all users' relevant people lists";
+    "10465720-29dd-4523-a11a-6a75c743c9d9" = "Read User Files";
+    "df85f4d6-205c-4ac5-a5ea-6bf408dba283" = "Read all files that user can access";
+    "5447fe39-cb82-4c1a-b977-520e67e724eb" = "Read files that the user selects (preview)";
+    "5c28f0bf-8a70-41f1-8ab2-9032436ddb65" = "Have full access to user files";
+    "863451e7-0667-486c-a5d6-d135439485f0" = "Have full access to all files user can access";
+    "8019c312-3263-48e6-825e-2b833497195b" = "Have full access to the application's folder (preview)";
+    "17dde5bd-8c17-420f-a486-969730c1b827" = "Read and write files that the user selects (preview)";
+    "9d822255-d64d-4b7a-afdb-833b9a97ed02" = "Create user OneNote notebooks";
+    "371361e4-b9e2-4a3f-8315-2a301a3b0a3d" = "Read user OneNote notebooks";
+    "dfabfca6-ee36-4db2-8208-7a28381419b3" = "Read all OneNote notebooks that user can access";
+    "615e26af-c38a-4150-ae3e-c3b0d4cb1d6a" = "Read and write user OneNote notebooks";
+    "64ac0503-b4fa-45d9-b544-71a463f05da0" = "Read and write all OneNote notebooks that user can access";
     "0e263e50-5827-48a4-b97c-d940288653c7" = "Access directory as the signed-in user";
     # Add more potentially malicious permissions as needed
 }
